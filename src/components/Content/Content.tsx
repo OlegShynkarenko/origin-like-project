d42d503cbcdb7679935f7fae225df86cef7c22a9
oshynkarenko <oshynkarenko@atlassian.com>
2020-03-30 09:56:43 +0300

    style: add basic colors obj

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import colors from "../../utils/colors";
 
 export const Content: React.FC = props => {
   return (
-    <div style={{ color: "#000", background: "#e5e8f0" }}>{props.children}</div>
+    <div style={{ color: colors.N800, background: colors.N20 }}>
+      {props.children}
+    </div>
   );
 };
