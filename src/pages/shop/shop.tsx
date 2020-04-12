d7c72e5d7c150ff473f3d23ed2418d98f992eb5a
oshynkarenko <oshynkarenko@atlassian.com>
2020-04-12 12:31:26 +0300

    refactor: make card as separate component

--- src/pages/shop/shop.tsx
+++ src/pages/shop/shop.tsx
@@ -2,34 +2,21 @@ import React from "react";
 import * as styles from "./styles";
 import { connect } from "react-redux";
 import { State, Game } from "@store/reducers/getGamesReducer";
-import gameTitleSrc from "../../assets/division.png";
-import appleLogo from "../../assets/apple.svg";
-import linuxLogo from "../../assets/linux.svg";
-import windowsLogo from "../../assets/windows.svg";
+import { Card } from "../../components/Card/card";
 
 const ShopComponent: React.FC = (props: any) => {
   const renderCards = () => {
     return props.games.map((el: Game) => {
       return (
-        <styles.cardWrapper key={el.id}>
-          <img
-            style={{ maxWidth: "100%" }}
-            src={gameTitleSrc}
-            alt="Game title"
-          />
-          <div style={{ textOverflow: "ellipsis", maxWidth: "100%" }}>
-            <styles.title>{el.title}</styles.title>
-            <styles.category>{`Category: ${el.category}`}</styles.category>
-          </div>
-          <styles.priceAndOsWrapper>
-            <styles.supportedOsWrapper>
-              {el.worksOnWindows && <img src={windowsLogo} />}
-              {el.worksOnMac && <img src={appleLogo} />}
-              {el.worksOnLinux && <img src={linuxLogo} />}
-            </styles.supportedOsWrapper>
-            <styles.price>{`$ ${el.price}`}</styles.price>
-          </styles.priceAndOsWrapper>
-        </styles.cardWrapper>
+        <Card
+          key={el.id}
+          title={el.title}
+          category={el.category}
+          worksOnWindows={el.worksOnWindows}
+          worksOnMac={el.worksOnMac}
+          worksOnLinux={el.worksOnLinux}
+          price={el.price}
+        />
       );
     });
   };
